cfc2c5c908d13e882a83b6d241947832fea36e91
liuyunfeng2011 <liuyunfeng2011@gmail.com>
2019-04-12 17:54:05 +0800

    1:jpython的支持 2:pdf java后台生成PDF的实验

--- src/main/java/com/mvcAnnotation/WebInitializer.java
+++ src/main/java/com/mvcAnnotation/WebInitializer.java
@@ -9,22 +9,30 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
 /**
  * 替代web.xml文件
+ * 
  * @author snake
  *
  */
-public class WebInitializer implements WebApplicationInitializer{
+public class WebInitializer implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		// TODO Auto-generated method stub
-		AnnotationConfigWebApplicationContext ctx=new AnnotationConfigWebApplicationContext();
-		//Spring mvc的配置文件
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		// Spring mvc的配置文件
 		ctx.register(Config.class);
+		// ctx.setConfigLocation("classpath*:/spring-mvc.xml"); 调用本地xml配置文件相当于
+//		<context-param>
+//		<param-name>contextConfigLocation</param-name>
+//		<param-value>classpath*:/applicationContext.xml
+//		</param-value>
+//	</context-param>
 		ctx.setServletContext(servletContext);
 		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
-        servlet.addMapping("/");  
-        servlet.setLoadOnStartup(1);  
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
 	}
 
 }
--- src/main/java/com/mvcAnnotation/controller/TestController.java
+++ src/main/java/com/mvcAnnotation/controller/TestController.java
@@ -1,7 +1,18 @@
 package com.mvcAnnotation.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.annotation.Resource;
 
+import org.python.core.Py;
+import org.python.core.PyFunction;
+import org.python.core.PyInteger;
+import org.python.core.PyObject;
+import org.python.core.PySystemState;
+import org.python.util.PythonInterpreter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +27,42 @@ public class TestController {
 	  @ResponseBody
 	 @RequestMapping(value = "sayHello.do", method = RequestMethod.GET)
 	public String sayHello() {
+		// System.setProperty("python.home","/Users/yexianyong/jython2.7.0/bin");
+		  PySystemState sys = Py.getSystemState(); 
+		  sys.path.add("/Users/yexianyong/jython2.7.0/Lib");
+		  // sys.path.add("F:\\Python27\\Lib\\site-packages\\jieba");  添加第三方库
+		 PythonInterpreter interp = new PythonInterpreter();
+		    // 执行Python程序语句
+		    interp.exec("import sys");
+		    interp.set("a",new PyInteger(42));
+		    interp.exec("print a");
+		    interp.exec("x = 2+2");
+		    PyObject x = interp.get("x");
+		    System.out.println("x: " + x);
+		    interp.exec("print sys.path");
 		return "hello world";
 	}
+	  @ResponseBody
+		 @RequestMapping(value = "sayHello2.do", method = RequestMethod.GET)
+	  public String sayHello2() {
+          PySystemState sys = Py.getSystemState(); 
+		  sys.path.add("/Users/yexianyong/jython2.7.0/Lib");
+		  sys.path.add("/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages");
+		  sys.path.add("/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/jieba");
+			 PythonInterpreter interp = new PythonInterpreter();
+			 interp.exec("import sys");
+			    // 执行Python程序语句
+			    PyObject res=null;
+			    try {
+			    	InputStream filepy = new FileInputStream("/Users/yexianyong/Desktop/sayHello 17.07.17.py");
+			    	interp.execfile(filepy);
+					PyFunction pyf = interp.get("sayHello", PyFunction.class);
+					 res = pyf.__call__(Py.newInteger(1), Py.newInteger(3));
+					 System.out.println(res);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}   
+			return "hello world";
+		}
 }
